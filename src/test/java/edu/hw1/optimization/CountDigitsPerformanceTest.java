d57f1dae960c1122312b8f1668be15a7f4a0dcaa
Bezukh <91319870+BezukhVladimir@users.noreply.github.com>
2023-10-07 22:52:48 +0500

    feat: add CountDigits performance test

---
+++ src/test/java/edu/hw1/optimization/CountDigitsPerformanceTest.java
@@ -0,0 +1,45 @@
+package edu.hw1.optimization;
+
+import edu.hw1.NumberUtils;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ * The countDigits() method is about 10 times more performant than the classicCountDigits() method.
+ * We should use countDigits() method. More precise performance testing is not required.
+ */
+public class CountDigitsPerformanceTest {
+    @Test
+    @DisplayName("Executing countDigits() method for all 32-bit decimal integer numbers")
+    void testCountDigits() {
+        // Arrange
+        int startNumber = Integer.MIN_VALUE;
+        int endNumber = Integer.MAX_VALUE;
+
+        // Act
+        for (int i = startNumber; ; ++i) {
+            int result = NumberUtils.countDigits(i);
+
+            if (i == endNumber) {
+                break;
+            }
+        }
+    }
+
+    @Test
+    @DisplayName("Executing classicCountDigits() method for all 32-bit decimal integer numbers")
+    void testClassicCountDigits() {
+        // Arrange
+        int startNumber = Integer.MIN_VALUE;
+        int endNumber = Integer.MAX_VALUE;
+
+        // Act
+        for (int i = startNumber; ; ++i) {
+            int result = NumberUtils.classicCountDigits(i);
+
+            if (i == endNumber) {
+                break;
+            }
+        }
+    }
+}
