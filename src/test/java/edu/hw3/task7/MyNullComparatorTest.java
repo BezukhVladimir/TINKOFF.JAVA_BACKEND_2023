624699b53fd3a1aed0eec4e0bf139f830645b0d4
Bezukh <91319870+BezukhVladimir@users.noreply.github.com>
2023-10-22 18:14:26 +0500

    feat: added Homework3 Task7 solution

---
+++ src/test/java/edu/hw3/task7/MyNullComparatorTest.java
@@ -0,0 +1,21 @@
+package edu.hw3.task7;
+
+import java.util.TreeMap;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MyNullComparatorTest {
+    @Test
+    public void testNull() {
+        // Arrange
+        TreeMap<String, String> tree = new TreeMap<>(
+            new MyNullComparator<String>()
+        );
+
+        // Act
+        tree.put(null, "test");
+
+        // Assert
+        assertThat(tree.containsKey(null)).isTrue();
+    }
+}
