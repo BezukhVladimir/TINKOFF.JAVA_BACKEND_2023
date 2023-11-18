dd61450da86cd894c6e6de51656f239532988d1d
Bezukh <91319870+BezukhVladimir@users.noreply.github.com>
2023-11-19 02:49:06 +0500

    feat: added LogsReporter

---
+++ src/main/java/edu/project3/logsreporter/reports/AdocReport.java
@@ -0,0 +1,8 @@
+package edu.project3.logsreporter.reports;
+
+// https://thetimetube.herokuapp.com/asciidoc/
+public final class AdocReport extends AbstractReport {
+    public AdocReport(String report) {
+        super(report);
+    }
+}
