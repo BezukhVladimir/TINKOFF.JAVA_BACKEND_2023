8f0182e91087d716c8a3694a362cb41cefde91d7
Bezukh <91319870+BezukhVladimir@users.noreply.github.com>
2023-11-27 17:27:29 +0500

    fix: now the threads run simultaneously

--- src/test/java/edu/hw7/task1/MultiThreadedVersionTest.java
+++ src/test/java/edu/hw7/task1/MultiThreadedVersionTest.java
@@ -3,6 +3,7 @@ package edu.hw7.task1;
 import edu.hw7.task1.multi.Counter;
 import org.junit.jupiter.api.Test;
 import java.util.concurrent.CountDownLatch;
+import static java.lang.Thread.sleep;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class MultiThreadedVersionTest {
@@ -20,29 +21,35 @@ public class MultiThreadedVersionTest {
     }
 
     @Test
-    public void testMultiThreadedIncrement() {
+    public void testMultiThreadedIncrement() throws InterruptedException {
         // Arrange
         var count = new Counter(0);
         int numberOfThreads = Runtime.getRuntime().availableProcessors() - 1;
+        var threads = new Thread[numberOfThreads];
         int iterationsPerThread = 1_000;
         int expected = numberOfThreads * iterationsPerThread;
         var latch = new CountDownLatch(numberOfThreads);
 
         // Act
         for (int i = 0; i < numberOfThreads; ++i) {
-            new Thread(() -> {
+            threads[i] = new Thread(() -> {
+                latch.countDown();
+
+                try {
+                    latch.await();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+
                 for (int j = 0; j < iterationsPerThread; ++j) {
                     count.increment();
                 }
-                latch.countDown();
-            }).start();
+            });
+            threads[i].start();
         }
 
-        try {
-            latch.await();
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-            throw new RuntimeException(e);
+        for (var thread : threads) {
+            thread.join();
         }
 
         // Assert
